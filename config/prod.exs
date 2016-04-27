2bf522d31142e0f6bed53f71a9cb5224fb5e4fcf
Thomas Poinsot <thomas.poinsot1@gmail.com>
2016-04-27 11:31:31 +0200

    Basic Heroku Config

--- config/prod.exs
+++ config/prod.exs
@@ -13,11 +13,20 @@ use Mix.Config
 # which you typically run after static files are built.
 config :cdtesting, Cdtesting.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  url: [scheme: "https", host: "cd-sandbox.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
+
+# Configure your database
+config :hello_phoenix, HelloPhoenix.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: 20,
+  ssl: true
 
 # ## SSL Support
 #
